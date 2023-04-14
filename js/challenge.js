d4e76c102f9c93a1f606eaed44f7fb0a42e4e8e8
Kennedy Muinde <kennedymuinde@gmail.com>
2023-04-14 11:57:08 +0300

    push for TM

--- js/challenge.js
+++ js/challenge.js
@@ -1,6 +1,9 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const counter = document.getElementById("counter");
+const counter = document.getElementById("counter");
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const heart = document.getElementById("heart");
 
+window.addEventListener("DOMContentLoaded", () => {
   // function to increment counter
   function incrementCounter() {
     counter.textContent++;
@@ -10,25 +13,47 @@ window.addEventListener("DOMContentLoaded", () => {
   const interval = setInterval(incrementCounter, 1000);
 
   //function to minus counter by 1
-  const minus = document.getElementById("minus");
   minus.addEventListener("click", () => {
     counter.textContent--;
   });
 
   //function to plus counter by 1
-  const plus = document.getElementById("plus");
   plus.addEventListener("click", () => {
     counter.textContent++;
   });
 
   //function to like the counter
-  const heart = document.getElementById("heart");
-  const ul = document.querySelector(".likes");
+
   heart.addEventListener("click", () => {
+    const ul = document.querySelector(".likes");
+    const snap = counter.textContent;
+    const likedNum = [...ul.children].map((a) => a.dataset.num);
+    if (likedNum.includes(snap)) {
+      console.log(likedNum);
+    } else {
+    }
     const li = document.createElement("li");
     li.textContent = counter.textContent;
     ul.appendChild(li);
   });
+
+
+ /*  heart.addEventListener("click", () => {
+   
+    let counterValue = counter.textContent;;
+    const likedItems = toArray(likesList.children).map(item => parseInt(item.dataset.num));
+    if (likedItems.includes(counterValue)) {
+      const likedItem = document.querySelector(`[data-num="${counterValue}"]`);
+      let likedCount = parseInt(likedItem.children[0].innerText);
+      likedItem.innerHTML = `${counterValue} has been liked <span>${likedCount + 1}</span> times`;
+    } else {
+      const newItem = document.createElement("li");
+      newItem.setAttribute("data-num", counterValue);
+      newItem.innerHTML = `${counterValue} has been liked <span>1</span> time`;
+      likesList.appendChild(newItem);
+    }
+  }); */
+
 
   //Function to add comment
   const commentDisplay = document.querySelector(".comments");
@@ -37,7 +62,10 @@ window.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     const comment = form.querySelector("#comment-input");
     const p = document.createElement("p");
-    p.textContent = comment.value;
+    const snap = comment.value;
+    console.log(snap);
+    p.textContent = snap;
+
     commentDisplay.appendChild(p);
     comment.value = "";
   });
